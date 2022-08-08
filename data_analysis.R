ab48996ca15c60af7a2214233be45a41b4e7622b
Michael Malto <michaeldm233@gmail.com>
2022-08-08 11:55:20 +0100

    added csv, added dataframe

--- data_analysis.R
+++ data_analysis.R
@@ -1,10 +1,24 @@
-# Create a data frame
-Data_Frame <- data.frame (
-  Training = c("Strength", "Stamina", "Other"),
-  Pulse = c(100, 150, 120),
-  Duration = c(60, 30, 45)
+weather_features <- read.csv("weather_features.csv")
+
+dt_iso <- weather_features["dt_iso"]
+head("dt_iso")
+
+Madrid_subset <- weather_features[grep("Madrid", weather_features$city_name), ]
+# head(Madrid_subset$temp)
+
+Valencia_subset <- weather_features[grep("Valencia", weather_features$city_name), ]
+# head(Valencia_subset$temp)
+
+Bilbao_subset <- weather_features[grep("Bilbao", weather_features$city_name), ]
+# head(Bilbao_subset$temp)
+
+
+cities_df <- data.frame (
+  temp_Madrid = Madrid_subset$temp,
+  temp_Valencia = Valencia_subset$temp,
+  temp_Bilbao = Bilbao_subset$temp
+  # temp_min_Madrid = Madrid_subset$temp_min,
+  # pressure_Madrid = Madrid_subset$temp_max
 )
 
-# Print the data frame
-Data_Frame
-summary(Data_Frame)
+head(cities_df)
