05bbc934608f85661768ed5c0c374d2c89c6d9ca
Michael Malto <michaeldm233@gmail.com>
2022-08-08 11:56:19 +0100

    add all columns

--- data_analysis.R
+++ data_analysis.R
@@ -1,24 +1,31 @@
 weather_features <- read.csv("weather_features.csv")
 
-dt_iso <- weather_features["dt_iso"]
-head("dt_iso")
-
 Madrid_subset <- weather_features[grep("Madrid", weather_features$city_name), ]
-# head(Madrid_subset$temp)
-
 Valencia_subset <- weather_features[grep("Valencia", weather_features$city_name), ]
-# head(Valencia_subset$temp)
-
 Bilbao_subset <- weather_features[grep("Bilbao", weather_features$city_name), ]
-# head(Bilbao_subset$temp)
+
 
 
 cities_df <- data.frame (
+  dt_iso_Madrid = Madrid_subset$dt_iso,
   temp_Madrid = Madrid_subset$temp,
-  temp_Valencia = Valencia_subset$temp,
-  temp_Bilbao = Bilbao_subset$temp
-  # temp_min_Madrid = Madrid_subset$temp_min,
-  # pressure_Madrid = Madrid_subset$temp_max
+  temp_min_Madrid = Madrid_subset$temp_min,
+  temp_max_Madrid = Madrid_subset$temp_max,
+  pressure_Madrid = Madrid_subset$pressure,
+  humidity_Madrid = Madrid_subset$humidity,
+  wind_speed_Madrid = Madrid_subset$wind_speed,
+  wind_deg_Madrid = Madrid_subset$wind_deg,
+  rain_1h_Madrid = Madrid_subset$rain_1h,
+  rain_3h_Madrid = Madrid_subset$rain_3h,
+  snow_3h_Madrid = Madrid_subset$snow_3h,
+  clouds_all_Madrid = Madrid_subset$clouds_all,
+  weather_id_Madrid = Madrid_subset$weather_id,
+  weather_main_Madrid = Madrid_subset$weather_main,
+  weather_description_Madrid = Madrid_subset$weather_description,
+  weather_icon_Madrid = Madrid_subset$weather_icon
 )
 
+# head(Valencia_subset$temp)
+
+# head(Bilbao_subset$temp)
 head(cities_df)
